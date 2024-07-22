22f449b615dd9a44b437fe7684b53aadfdf9b478
mustfa21 <mmuu21itn@gmail.com>
2024-07-22 17:04:38 +0300

    1

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,7 +1,6 @@
 @extends('frontend.site.app')
 
 @section('content')
-
     <!-- Page header Start -->
     <div class="page-header parallaxie">
         <div class="container">
@@ -36,20 +35,30 @@
                             <p class="wow fadeInUp" data-wow-delay="0.5s"></p>
                         </div>
                         <div class="get-quote-form wow fadeInUp" data-wow-delay="0.75s">
-                            <form id="contactForm" action="#" method="post" data-toggle="validator">
+                            <form action="{{ route('contact_us') }}" method="post">
+                                @csrf
                                 <div class="row">
                                     <div class="form-group col-md-12 mb-3">
-                                        <input type="text" class="form-control" id="name" placeholder="الاسم" required="">
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-
-                                    <div class="form-group  col-md-12 mb-3">
-                                        <input type="email" class="form-control" id="email" placeholder="البريد الكتروني" required="">
+                                        <input type="text" class="form-control" name="name" id="name"
+                                            placeholder="الاسم" required="">
                                         <div class="help-block with-errors"></div>
                                     </div>
 
                                     <div class="form-group col-md-12 mb-3">
-                                        <textarea name="msg" class="form-control" id="msg" rows="3" placeholder="الرساله" required=""></textarea>
+                                        <input type="text" class="form-control" name="phone" id="phone"
+                                            placeholder="رقم الجوال" required="">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+
+                                    <div class="form-group  col-md-12 mb-3">
+                                        <input type="email" class="form-control" name="email" id="email"
+                                            placeholder="البريد الكتروني" required="">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="form-group col-md-12 mb-3">
+                                        <textarea class="form-control" id="msg" name="message" rows="3" placeholder="الرساله" required=""></textarea>
                                         <div class="help-block with-errors"></div>
                                     </div>
 
@@ -71,7 +80,7 @@
                         <div class="contact-us-info">
                             <div class="footer-info-box wow fadeInUp" data-wow-delay="0.25s">
                                 <div class="icon-box">
-                                    <img src="{{asset('assets/images/icon-mail.svg')}}" alt="">
+                                    <img src="{{ asset('assets/images/icon-mail.svg') }}" alt="">
                                 </div>
 
                                 <p>info@cotamayozalnajah.com</p>
@@ -79,7 +88,7 @@
 
                             <div class="footer-info-box wow fadeInUp" data-wow-delay="0.5s">
                                 <div class="icon-box">
-                                    <img src="{{asset('assets/images/icon-location.svg')}}" alt="">
+                                    <img src="{{ asset('assets/images/icon-location.svg') }}" alt="">
                                 </div>
 
                                 <p>المملكة العربيه السعوديه الرياض</p>
@@ -87,7 +96,7 @@
 
                             <div class="footer-info-box wow fadeInUp" data-wow-delay="0.75s">
                                 <div class="icon-box">
-                                    <img src="{{asset('assets/images/icon-call.svg')}}" alt="">
+                                    <img src="{{ asset('assets/images/icon-call.svg') }}" alt="">
                                 </div>
 
                                 <p>0553383310</p>
@@ -99,14 +108,24 @@
                         <div class="contact-us-footer wow fadeInUp" data-wow-delay="1s">
                             <div class="topbar-social-links contact-details">
                                 <ul>
-                                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                    <li><a href="https://www.facebook.com/profile.php?id=61558719242354"><i
+                                                class="fa-brands fa-facebook-f"></i></a></li>
+                                    <li><a href="https://x.com/shrkttmyzalnjah"><i class="fa-brands fa-twitter"></i></a>
+                                    </li>
+                                    <li><a href="https://www.instagram.com/shrkttmyzlnjaht.m.n?igsh=NWk1ZmxtNmNka3lm"><i
+                                                class="fa-brands fa-instagram"></i></a></li>
+                                    <li><a
+                                            href="https://www.snapchat.com/add/shrkttmyzlnjah?share_id=c7GVADFJZxM&locale=ar-SA
+"><i
+                                                class="fa-brands fa-snapchat"></i></a></li>
+
                                 </ul>
                             </div>
                             <!--Get Contact Us Start-->
                             <div class="header-contact-us">
-                                <a href="#"><img src="{{asset('assets/images/icon-phone.svg')}}" alt=""><h3>توصل معنا الان <span>0553383310</span></h3></a>
+                                <a href="#"><img src="{{ asset('assets/images/icon-phone.svg') }}" alt="">
+                                    <h3>توصل معنا الان <span>0553383310</span></h3>
+                                </a>
                             </div>
                             <!--Get Contact Us End-->
                         </div>
@@ -124,12 +143,14 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="google-map-iframe">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14516.019250769436!2d46.5941501!3d24.5544958!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f111aede8f4ed%3A0xbb9d460e166cc220!2z2LTYsdmD2Kkg2KrZhdmK2LIg2KfZhNmG2KzYp9itINin2YTZhNmI2KzYs9iq2YrYqQ!5e0!3m2!1sen!2ssa!4v1721498698175!5m2!1sen!2ssa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                    </div>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14516.019250769436!2d46.5941501!3d24.5544958!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f111aede8f4ed%3A0xbb9d460e166cc220!2z2LTYsdmD2Kkg2KrZhdmK2LIg2KfZhNmG2KzYp9itINin2YTZhNmI2KzYs9iq2YrYqQ!5e0!3m2!1sen!2ssa!4v1721498698175!5m2!1sen!2ssa"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- Google Map Iframe End -->
-
-
-    @endsection
+@endsection
